cc554d5c391bcd0a57fa701c22bf228d05034c7a
unknown <git@github.com:LazarGaborovic/restaurant.git>
2022-07-21 11:31:25 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import Loading from './components/Loading';
 import Cartoon from './components/Cartoon';
 import axios from 'axios';
 import './App.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useState,Text } from 'react';
+import React from 'react';
 
 
 function App() {
 
-    
+   
 
   return (
-
-    <Cartoon />
-    
+    <div>     
+    <Loading /> 
+    <Cartoon/>
+    </div>
   );
 }
+
 
 export default App;
--- src/components/Cartoon.js
+++ src/components/Cartoon.js
@@ -1,36 +1,98 @@
-import React, { useEffect, useState } from 'react'
-import Loading from './Loading';
-import axios from 'axios';
-import './Cartoon.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./Cartoon.css";
+import { Container, Row, CardBody, Card, CardTitle, Col } from "reactstrap";
+
+
 const Cartoon = () => {
+  const [results, setResults] = useState([]);
+  const [characters, setCharacters] = useState([]);
+  const [info, setInfo] = useState([]);
+  const [search, setSearch] = useState("");
 
-    const [characters, SetCharacters] = useState([]);
+  useEffect(() => {
+    console.log('info :', info);
+    console.log('search: ', search);
+    console.log('results: ', results);
+  },[ info, search, results]);
 
-    useEffect (() => {
-  
-        const fetchData = async () => {
-          try {
-              const {data} = await axios.get("https://rickandmortyapi.com/api/character");
-              SetCharacters(data.results);
-          } catch {
-            console.log();
-          }
-        }
-  
-        fetchData();
-    }, [])
-  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get(
+          "https://rickandmortyapi.com/api/character?page=1"
+        );
+        setInfo(data.info);
+        setSearch(data.search)
+        setCharacters(data.results);
+        setResults(data.results)
+      } catch {
+        console.log();
+      }
+    };
+
+    fetchData();
+  }, []); 
   return (
-    <div className='results'>  
-    {characters.map(character => (
-      <div>
-        <img src={character.image} alt={character.name} />
-        {character.name}
-         </div>
-    ))}
-  
-  </div>   
-  )
-}
+    <div className="cartoon_main mt-6" >
+      <form>
+        <input onChange={(e) => {
+          setSearch(e.value.target)
+        }}
+        value={search}
+        type="text" />
+        <button>Search</button>
+      </form>
+      <div className="card__cartoon" >
+          <Container >
+          <Row>
+            {characters.map((character, index) => {
+              return (
+                <Col sm="3" className="cards" >
+               
+                  <Card className="cartoon__cards m-2 bg-dark">
+                    <img src={character.image} alt={results.name} 
+                    />  
+                    <CardBody className="characteristics">
+                      <CardTitle>
+                        <h1>{character.name}</h1>
+                      </CardTitle>   
+                      <p key={index}>{character.location.name}</p>
+                      {(() => {
+                        if(character.status === "Dead"){
+                          return ( <div className="badge  text-bg-danger">{character.status}</div>
+                          );
+                        }
+                        else if(character.status === "Alive"){
+                          return (
+                            <div className="  badge text-bg-success">{character.status}</div>
+                          );
+                        }
+                        else {
+                          return (
+                            <div className="  badge  text-bg-secondary">{character.status}</div>
+                          );
+                        }
+                      })()};
+                               
+                      -
+                      <span> {character.gender}</span>
+                      <p>{character.species}</p>
+                      <p>{character.type}</p>
+                  
+                      
+                    </CardBody>
 
-export default Cartoon
+                   
+                  </Card>
+                </Col>
+              );
+            })}
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default Cartoon;
